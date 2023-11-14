21b679aa26a4efef548bb08b84ac5874a564bc96
Hemanth-Kagitha <123528403+Hemanth-Kagitha@users.noreply.github.com>
2023-11-13 17:45:46 -0800

    Add files via upload

---
+++ src/main/java/com/hemanthClasses/DMS/repository/TrainerRespository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.hemanthClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hemanthClasses.DMS.Models.Trainer;
+
+/**
+ * @author Hemanth Kagitha 
+ * Nov 13, 2023
+ */
+public interface TrainerRespository extends CrudRepository<Trainer, Integer> {
+
+}
